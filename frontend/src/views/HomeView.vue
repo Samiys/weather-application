61d46a55952940780c7583a1d3b5288b239610fd
Sami <samiys005@gmail.com>
2024-09-25 18:32:32 +0200

    Adding the home page component and configuring app.vue

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home-view">
+    <h1>Weather Information by City</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped>
+.home-view {
+  text-align: center;
+}
+</style>
