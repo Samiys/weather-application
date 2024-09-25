4c9eeee12c7d213a2e10cad47cf6dc059e5f78b8
Sami <samiys005@gmail.com>
2024-09-25 20:06:52 +0200

    updating backend api url

--- frontend/src/services/WeatherService.js
+++ frontend/src/services/WeatherService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:2000/';
+const API_URL = 'http://localhost:5000/';
 
 class WeatherService {
     async getWeatherData(latitude, longitude) {
