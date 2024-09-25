5d221424c0e0307dd2a2bbad37893821222417cd
Sami <samiys005@gmail.com>
2024-09-25 16:49:17 +0200

    implemented API to fetch weather data

--- backend/src/Providers/OpenWeatherProvider.php
+++ backend/src/Providers/OpenWeatherProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Exceptions\NotFoundException;
 use App\Interfaces\WeatherProviderInterface;
 
 class OpenWeatherProvider implements WeatherProviderInterface
@@ -15,7 +16,7 @@ class OpenWeatherProvider implements WeatherProviderInterface
     public function __construct()
     {
         if (empty($_ENV['WEATHER_API_KEY'])) {
-            throw new \Exception("WEATHER_API_KEY is not set.");
+            throw new NotFoundException("WEATHER_API_KEY is not set.");
         }
         $this->apiKey = $_ENV['WEATHER_API_KEY'];
     }
@@ -24,10 +25,23 @@ class OpenWeatherProvider implements WeatherProviderInterface
      * @param float $latitude
      * @param float $longitude
      * @return array
+     * @throws \JsonException
      */
     public function fetchWeatherByCoordinates(float $latitude, float $longitude): array
     {
-        // TODO: Implement API fetching logic
-        return [];
+        $url = "{$this->apiUrl}?lat={$latitude}&lon={$longitude}&appid={$this->apiKey}&units=metric";
+        $response = file_get_contents($url);
+        if ($response === false) {
+            throw new \RuntimeException("Error fetching weather data for coordinates: {$latitude}, {$longitude}");
+        }
+
+        $data = json_decode($response, true, 512, JSON_THROW_ON_ERROR);
+        return [
+            'city' => $data['name'],
+            'min_temp' => $data['main']['temp_min'],
+            'max_temp' => $data['main']['temp_max'],
+            'description' => $data['weather'][0]['description'],
+            'icon' => $data['weather'][0]['icon'],
+        ];
     }
 }
