a08104dcae479847021e497622efc703bf592224
Sami <samiys005@gmail.com>
2024-09-25 15:35:03 +0200

    loading methods in index and adding dotenv to load env variables

--- backend/index.php
+++ backend/index.php
@@ -3,3 +3,19 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+use App\Controllers\WeatherController;
+use App\Providers\OpenWeatherProvider;
+use App\Services\WeatherService;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$weatherProvider = new OpenWeatherProvider();
+$weatherService = new WeatherService($weatherProvider);
+$weatherController = new WeatherController($weatherService);
+
+$latitude = 50.5859482420135000;
+$longitude = 8.6730142590698300;
+
+$cities = $weatherController->showWeatherData($latitude, $longitude);
