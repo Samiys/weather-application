19317c638fddaefc0e20575233e4fb08d8620777
Sami <samiys005@gmail.com>
2024-09-25 15:34:03 +0200

    loading data from file

--- backend/src/Controllers/WeatherController.php
+++ backend/src/Controllers/WeatherController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\City;
 use App\Services\WeatherService;
 
 class WeatherController
@@ -15,9 +16,44 @@ class WeatherController
 
     public function showWeatherData($latitude, $longitude): array
     {
-        // TODO: Load data from file
+        $cities = $this->loadCitiesFromFile();
+        foreach ($cities as $city) {
+            print_r($city);
+        }
         // TODO: Fetch data from Weather API
         // TODO: Calculate distance
+
+        return $cities;
+    }
+
+    private function loadCitiesFromFile(): array
+    {
+        $cities = [];
+        $filePath = __DIR__ . '/../data/cities.dat';
+        $file = fopen($filePath, 'r');
+
+        if ($file === false) {
+            throw new \RuntimeException('Failed to open cities.dat file.');
+        }
+
+        $header = true;
+        while (($line = fgetcsv($file, 0, ' ')) !== false) {
+            if($header) {
+                $header = false;
+                continue;
+            }
+
+            if(count($line) >= 5) {
+                $cities[] = new City(
+                    trim($line[2]), // City name
+                    trim($line[0]), // Country code
+                    (float)trim($line[4]), // Latitude
+                    (float)trim($line[3])  // Longitude
+                );
+            }
+        }
+        fclose($file);
+        return $cities;
     }
 
 }
