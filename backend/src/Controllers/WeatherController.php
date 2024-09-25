693f053f537ed40446f5c02a1c9cce5968858ee0
Sami <samiys005@gmail.com>
2024-09-25 16:58:26 +0200

    returning weather api response for cities in file

--- backend/src/Controllers/WeatherController.php
+++ backend/src/Controllers/WeatherController.php
@@ -14,13 +14,21 @@ class WeatherController
         $this->weatherService = $weatherService;
     }
 
-    public function showWeatherData($latitude, $longitude): array
+    public function showWeatherData(float $latitude, float $longitude): array
     {
         $cities = $this->loadCitiesFromFile();
         foreach ($cities as $city) {
-            print_r($city);
+            $weatherData = $this->weatherService->getWeather($city->getLatitude(), $city->getLongitude());
+            $city->min_temp = $weatherData['min_temp'];
+            $city->max_temp = $weatherData['max_temp'];
+            $city->description = $weatherData['description'];
+            $city->icon = $weatherData['icon'];
+            $city->city = $city->getName();
         }
-        // TODO: Fetch data from Weather API
+
+        usort($cities, function ($a, $b) {
+            return ($a->max_temp - $a->min_temp) <=> ($b->max_temp - $b->min_temp);
+        });
         // TODO: Calculate distance
 
         return $cities;
@@ -31,7 +39,6 @@ class WeatherController
         $cities = [];
         $filePath = __DIR__ . '/../data/cities.dat';
         $file = fopen($filePath, 'r');
-
         if ($file === false) {
             throw new \RuntimeException('Failed to open cities.dat file.');
         }
@@ -42,7 +49,6 @@ class WeatherController
                 $header = false;
                 continue;
             }
-
             if(count($line) >= 5) {
                 $cities[] = new City(
                     trim($line[2]), // City name
