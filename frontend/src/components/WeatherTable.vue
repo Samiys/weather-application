8c8b1920553c50422a10ed2216e5a3a17583f685
Sami <samiys005@gmail.com>
2024-09-25 19:25:02 +0200

    displaying the weather data in the table

---
+++ frontend/src/components/WeatherTable.vue
@@ -0,0 +1,59 @@
+<template>
+  <div v-if="cities.length" class="weather-table">
+    <h2>Weather Data</h2>
+    <table>
+      <thead>
+      <tr>
+        <th>City</th>
+        <th>Min Temp (°C)</th>
+        <th>Max Temp (°C)</th>
+        <th>Distance (km)</th>
+        <th>Weather</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="city in cities" :key="city.name">
+        <td>{{ city.city }}</td>
+        <td>{{ city.min_temp }} °C</td>
+        <td>{{ city.max_temp }} °C</td>
+        <td>{{ city.distance }} km</td>
+        <td>
+          <img :src="getIconUrl(city.icon)" alt="Weather Icon" />
+          {{ city.description }}
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+  <div v-else>
+    <p>No weather data available. Enter coordinates to get data.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['cities'],
+  methods: {
+    getIconUrl(icon) {
+      return `http://openweathermap.org/img/w/${icon}.png`;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.weather-table {
+  margin-top: 20px;
+}
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 10px;
+  border: 1px solid #ccc;
+}
+img {
+  vertical-align: middle;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,22 +1,35 @@
 <template>
   <div class="home-view">
     <h1>Weather Information by City</h1>
-    <weather-form/>
+    <weather-form @get-weather="fetchWeatherData"/>
+    <weather-table :cities="cities" />
   </div>
 </template>
 
 <script>
 import WeatherForm from '@/components/WeatherForm.vue';
+import WeatherService from "@/services/WeatherService";
+import WeatherTable from '@/components/WeatherTable.vue';
 
 export default {
   components: {
     WeatherForm,
+    WeatherTable
   },
   data() {
     return {
+      cities: []
     };
   },
   methods: {
+    async fetchWeatherData({latitude, longitude}) {
+      try {
+        const data = await WeatherService.getWeatherData(latitude, longitude);
+        this.cities = data;
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    }
   }
 };
 </script>
