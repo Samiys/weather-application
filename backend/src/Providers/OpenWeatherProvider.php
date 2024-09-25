02431a6b38cd13f09d119ee2f5f6ab4d5be162f2
Sami <samiys005@gmail.com>
2024-09-25 14:49:39 +0200

    constructing WeatherProvider for reusability

--- backend/src/Providers/OpenWeatherProvider.php
+++ backend/src/Providers/OpenWeatherProvider.php
@@ -2,7 +2,32 @@
 
 namespace App\Providers;
 
-class OpenWeatherProvider
-{
+use App\Interfaces\WeatherProviderInterface;
 
+class OpenWeatherProvider implements WeatherProviderInterface
+{
+    private string $apiKey;
+    private string $apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+    /**
+     * @throws \Exception
+     */
+    public function __construct()
+    {
+        if (empty($_ENV['WEATHER_API_KEY'])) {
+            throw new \Exception("WEATHER_API_KEY is not set.");
+        }
+        $this->apiKey = $_ENV['WEATHER_API_KEY'];
+    }
+
+    /**
+     * @param float $latitude
+     * @param float $longitude
+     * @return array
+     */
+    public function fetchWeatherByCoordinates(float $latitude, float $longitude): array
+    {
+        // TODO: Implement API fetching logic
+        return [];
+    }
 }
