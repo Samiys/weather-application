2a2487f325a6b8ed3369219aee3ce5fd5214b8a3
Sami <samiys005@gmail.com>
2024-09-25 18:57:27 +0200

    adding form to add Latitude and Longitude

---
+++ frontend/src/components/WeatherForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="weather-form">
+    <h2>Enter Coordinates</h2>
+    <form @submit.prevent="submitForm">
+      <label for="latitude">Latitude:</label>
+      <input type="text" v-model="latitude" placeholder="Enter latitude" required />
+
+      <label for="longitude">Longitude:</label>
+      <input type="text" v-model="longitude" placeholder="Enter longitude" required />
+
+      <button type="submit">Get Weather</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      latitude: '',
+      longitude: ''
+    };
+  },
+  methods: {
+    submitForm() {
+      this.$emit('get-weather', { latitude: this.latitude, longitude: this.longitude });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.weather-form {
+  margin-bottom: 20px;
+}
+label {
+  margin-right: 10px;
+}
+input {
+  margin-right: 10px;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="home-view">
     <h1>Weather Information by City</h1>
+    <weather-form/>
   </div>
 </template>
 
 <script>
+import WeatherForm from '@/components/WeatherForm.vue';
 
 export default {
   components: {
+    WeatherForm,
   },
   data() {
     return {
