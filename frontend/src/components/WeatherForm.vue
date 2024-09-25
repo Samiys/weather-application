c8bf7e883e64bf05cf0e257ccde9ff62ae8d507e
Sami <samiys005@gmail.com>
2024-09-25 20:06:30 +0200

    adding validation to the weather form

--- frontend/src/components/WeatherForm.vue
+++ frontend/src/components/WeatherForm.vue
@@ -3,13 +3,26 @@
     <h2>Enter Coordinates</h2>
     <form @submit.prevent="submitForm">
       <label for="latitude">Latitude:</label>
-      <input type="text" v-model="latitude" placeholder="Enter latitude" required />
+      <input
+          type="text"
+          v-model="latitude"
+          placeholder="Enter latitude"
+          @input="validateLatitude"
+          required
+      />
 
       <label for="longitude">Longitude:</label>
-      <input type="text" v-model="longitude" placeholder="Enter longitude" required />
+      <input
+          type="text"
+          v-model="longitude"
+          placeholder="Enter longitude"
+          @input="validateLongitude"
+          required
+      />
 
       <button type="submit">Get Weather</button>
     </form>
+    <div v-if="error" class="error-message">{{ error }}</div>
   </div>
 </template>
 
@@ -18,12 +31,33 @@ export default {
   data() {
     return {
       latitude: '',
-      longitude: ''
+      longitude: '',
+      error: null
     };
   },
   methods: {
+    validateLatitude() {
+      const regex = /^-?\d*(\.\d*)?$/;
+      if (!regex.test(this.latitude)) {
+        this.error = 'Latitude must be a valid number';
+        this.latitude = this.latitude.slice(0, -1);
+      } else {
+        this.error = null;
+      }
+    },
+    validateLongitude() {
+      const regex = /^-?\d*(\.\d*)?$/;
+      if (!regex.test(this.longitude)) {
+        this.error = 'Longitude must be a valid number';
+        this.longitude = this.longitude.slice(0, -1);
+      } else {
+        this.error = null;
+      }
+    },
     submitForm() {
-      this.$emit('get-weather', { latitude: this.latitude, longitude: this.longitude });
+      if (!this.error) {
+        this.$emit('get-weather', {latitude: this.latitude, longitude: this.longitude});
+      }
     }
   }
 };
@@ -33,10 +67,21 @@ export default {
 .weather-form {
   margin-bottom: 20px;
 }
+
 label {
   margin-right: 10px;
 }
+
 input {
   margin-right: 10px;
+}
+
+button {
+  margin-top: 10px;
+}
+
+.error-message {
+  color: red;
+  margin-top: 10px;
 }
 </style>
