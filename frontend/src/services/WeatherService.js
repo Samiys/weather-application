2459f792fb48fcd16b2d540d446b595053d36f8f
Sami <samiys005@gmail.com>
2024-09-25 19:24:36 +0200

    adding service to fetch the weather data

---
+++ frontend/src/services/WeatherService.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:2000/';
+
+class WeatherService {
+    async getWeatherData(latitude, longitude) {
+        try {
+            const response = await axios.get(API_URL, {
+                params: {
+                    lat: latitude,
+                    lon: longitude
+                }
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+            throw error;
+        }
+    }
+}
+
+export default new WeatherService();
