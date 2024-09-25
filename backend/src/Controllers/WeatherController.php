4cb5f491b7f667a0a02940985c326493e4fbffeb
Sami <samiys005@gmail.com>
2024-09-25 14:50:27 +0200

    constructing WeatherController and Service for logic

--- backend/src/Controllers/WeatherController.php
+++ backend/src/Controllers/WeatherController.php
@@ -2,7 +2,22 @@
 
 namespace App\Controllers;
 
+use App\Services\WeatherService;
+
 class WeatherController
 {
+    private WeatherService $weatherService;
+
+    public function __construct(WeatherService $weatherService)
+    {
+        $this->weatherService = $weatherService;
+    }
+
+    public function showWeatherData($latitude, $longitude): array
+    {
+        // TODO: Load data from file
+        // TODO: Fetch data from Weather API
+        // TODO: Calculate distance
+    }
 
 }
--- backend/src/Services/WeatherService.php
+++ backend/src/Services/WeatherService.php
@@ -2,7 +2,20 @@
 
 namespace App\Services;
 
+use App\Interfaces\WeatherProviderInterface;
+
 class WeatherService
 {
+    private WeatherProviderInterface $weatherProvider;
+
+    public function __construct(WeatherProviderInterface $weatherProvider)
+    {
+        $this->weatherProvider = $weatherProvider;
+    }
+
+    public function getWeather()
+    {
+        //
+    }
 
 }
